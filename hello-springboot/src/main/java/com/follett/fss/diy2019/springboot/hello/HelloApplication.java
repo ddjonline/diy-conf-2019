331b39cb019f6d79783f74425cfc296b74a7ec62
Darren Jones <ddjones@follett.com>
2019-04-27 17:01:08 -0400

    spring-boot hello REST app

---
+++ hello-springboot/src/main/java/com/follett/fss/diy2019/springboot/hello/HelloApplication.java
@@ -0,0 +1,12 @@
+package com.follett.fss.diy2019.springboot.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
