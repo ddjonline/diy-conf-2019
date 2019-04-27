a4c6c1bda61e890561ba06b01dcf10e233b2245c
Darren Jones <ddjones@follett.com>
2019-04-27 17:24:21 -0400

    vertx hello app

---
+++ hello-vertx/src/main/java/com/follett/fss/diy2019/vertx/hello/MainVerticle.java
@@ -0,0 +1,45 @@
+package com.follett.fss.diy2019.vertx.hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+
+public class MainVerticle extends AbstractVerticle {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @Override
+    public void start() {
+        Router router = Router.router(vertx);
+        router.get("/hello").handler(context -> context.response().end("Hello DIYConf2019 (" + counter.incrementAndGet() + ")"));
+        router.get("/naptime").handler(context -> context.response().end("your slice of pi is " + pi_digits(20000)));
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+
+    }
+
+    private static String pi_digits(int digits) {
+        int scale = 10000;
+        int array_init = 2000;
+        StringBuffer pi = new StringBuffer();
+        int[] arr = new int[digits + 1];
+        int carry = 0;
+
+        for (int i = 0; i <= digits; ++i)
+            arr[i] = array_init;
+
+        for (int i = digits; i > 0; i -= 14) {
+            int sum = 0;
+            for (int j = i; j > 0; --j) {
+                sum = sum * j + scale * arr[j];
+                arr[j] = sum % (j * 2 - 1);
+                sum /= j * 2 - 1;
+            }
+
+            pi.append(String.format("%04d", carry + sum / scale));
+            carry = sum % scale;
+        }
+        return pi.toString();
+    }
+
+}
