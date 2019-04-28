d553ed16e16ad407a7c58f2d5d738240da7c5d91
Darren Jones <ddjones@follett.com>
2019-04-27 22:56:52 -0400

    hello wildfly (16.0.0.Final) app

---
+++ hello-wildfly/src/main/java/com/follett/fss/diy2019/wildfly/hello/JAXActivator.java
@@ -0,0 +1,9 @@
+package com.follett.fss.diy2019.wildfly.hello;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class JAXActivator extends Application {
+    // Left empty intentionally
+}
